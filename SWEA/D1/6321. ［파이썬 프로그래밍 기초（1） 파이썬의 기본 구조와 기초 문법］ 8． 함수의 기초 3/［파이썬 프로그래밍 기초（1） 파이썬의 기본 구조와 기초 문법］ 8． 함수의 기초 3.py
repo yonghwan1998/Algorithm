207e24e60b88df1892a4f67d7126a74c325d9cfb
yongss <129483177+yonghwan1998@users.noreply.github.com>
2026-07-19 08:59:13 +0900

    [D1] Title: [파이썬 프로그래밍 기초(1) 파이썬의 기본 구조와 기초 문법] 8. 함수의 기초 3, Time: 50 ms, Memory: 51,328 KB -BaekjoonHub

---
+++ SWEA/D1/6321. ［파이썬 프로그래밍 기초（1） 파이썬의 기본 구조와 기초 문법］ 8． 함수의 기초 3/［파이썬 프로그래밍 기초（1） 파이썬의 기본 구조와 기초 문법］ 8． 함수의 기초 3.py
@@ -0,0 +1,16 @@
+def is_prime(n):
+    if n < 2:
+        return False
+    
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+        
+    return True
+
+n = int(input())
+
+if is_prime(n):
+    print('소수입니다.')
+else:
+    print('소수가 아닙니다.')
