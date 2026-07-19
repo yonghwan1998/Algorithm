284671c21ebc08851e8b05358cd730d72591a30b
yongss <129483177+yonghwan1998@users.noreply.github.com>
2026-07-19 19:19:59 +0900

    [D1] Title: [파이썬 프로그래밍 기초(2) 파이썬의 기본 응용] 2. 자료구조 -리스트, 튜플 2, Time: 58 ms, Memory: 49,408 KB -BaekjoonHub

---
+++ SWEA/D1/6275. ［파이썬 프로그래밍 기초（2） 파이썬의 기본 응용］ 2． 자료구조 －리스트， 튜플 2/［파이썬 프로그래밍 기초（2） 파이썬의 기본 응용］ 2． 자료구조 －리스트， 튜플 2.py
@@ -0,0 +1,3 @@
+s_input = 'Python is powerful... and fast; plays well with others; runs everywhere; is friendly & easy to learn; is Open.'
+answer = s_input.replace('a', '').replace('e', '').replace('i', '').replace('o', '').replace('u', '')
+print(answer)
