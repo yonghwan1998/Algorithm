04515a80584d3d4ddfd249c613aafc1e2706a7e5
yongss <129483177+yonghwan1998@users.noreply.github.com>
2026-08-13 15:19:33 +0900

    [level 2] Title: 오픈채팅방, Time: 58.69 ms, Memory: 41.4 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.py
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.py
@@ -1,22 +1,20 @@
 def solution(record):
     answer = []
     
-    dict_uid = {}
+    dict_nicknames = {}
+    
+    for item in record:
+        parts = item.split()
+        
+        if parts[0] in {'Enter', 'Change'}:
+            dict_nicknames[parts[1]] = parts[2]
 
     for item in record:
-        input = list(map(str, item.split()))
+        parts = item.split()
         
-        if len(input) == 3:
-            dict_uid[input[1]] = input[2]
-            
-    for item in record:
-        input = list(map(str, item.split()))
-        action = input[0]
-        uid = input[1]
-        
-        if action == 'Enter':
-            answer.append(f'{dict_uid[uid]}님이 들어왔습니다.')
-        elif action == 'Leave':
-            answer.append(f'{dict_uid[uid]}님이 나갔습니다.')
+        if parts[0] == 'Enter':
+            answer.append(f'{dict_nicknames[parts[1]]}님이 들어왔습니다.')
+        elif parts[0] == 'Leave':
+            answer.append(f'{dict_nicknames[parts[1]]}님이 나갔습니다.')
     
     return answer
