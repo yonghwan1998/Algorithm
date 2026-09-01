d4671b0127b98694a915ebb9aef45bd3888981e2
yongss <129483177+yonghwan1998@users.noreply.github.com>
2026-09-01 16:28:27 +0900

    [D2] Title: 숫자 배열 회전, Time: 73 ms, Memory: 52,480 KB -BaekjoonHub

--- SWEA/D2/1961. 숫자 배열 회전/숫자 배열 회전.py
+++ SWEA/D2/1961. 숫자 배열 회전/숫자 배열 회전.py
@@ -1,34 +1,27 @@
 T = int(input())
 
-def rotate_board(board, N):
-    rotated_board = [[0 for _ in range(N)] for _ in range(N)]
-    for i in range(N):
-        for j in range(N):
-            rotated_board[j][N - 1 - i] = board[i][j]
-            
-    return rotated_board
+def rotate_matrix(N, matrix):
+    matrix_new = [[0 for _ in range(N)] for _ in range(N)]
+
+    for row in range(N):
+        for col in range(N):
+            matrix_new[col][N - row - 1] = matrix[row][col]
+    return matrix_new
 
 for test_case in range(1, T + 1):
-    N = int(input())
     
-    board = []
+    N = int(input())
+    matrix = []
     
     for _ in range(N):
-        board.append(list(map(int, input().split())))
-
-    rotated_board_90 = rotate_board(board, N)
-    rotated_board_180 = rotate_board(rotated_board_90, N)
-    rotated_board_270 = rotate_board(rotated_board_180, N)
+        matrix.append(list(map(int, input().split())))
+        
+    matrix_90 = rotate_matrix(N, matrix)
+    matrix_180 = rotate_matrix(N, matrix_90)
+    matrix_270 = rotate_matrix(N, matrix_180)
     
-    print(f"#{test_case}")
-    
+    print(f'#{test_case}')
     for i in range(N):
-        for j in range(N):
-            print(rotated_board_90[i][j], end='')
-        print('', end=' ')
-        for j in range(N):
-            print(rotated_board_180[i][j], end='')
-        print('', end=' ')
-        for j in range(N):
-            print(rotated_board_270[i][j], end='')
-        print()
+        print(*matrix_90[i], sep='', end=' ')
+        print(*matrix_180[i], sep='', end=' ')
+        print(*matrix_270[i], sep='')
