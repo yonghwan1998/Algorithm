d49c2de1a194753b53a052fe23b98ad8dfda84ec
yongss <129483177+yonghwan1998@users.noreply.github.com>
2026-09-01 15:47:46 +0900

    [D2] Title: 어디에 단어가 들어갈 수 있을까, Time: 67 ms, Memory: 53,504 KB -BaekjoonHub

--- SWEA/D2/1979. 어디에 단어가 들어갈 수 있을까/어디에 단어가 들어갈 수 있을까.py
+++ SWEA/D2/1979. 어디에 단어가 들어갈 수 있을까/어디에 단어가 들어갈 수 있을까.py
@@ -2,38 +2,38 @@ T = int(input())
 
 for test_case in range(1, T + 1):
     N, K = map(int, input().split())
-    
-    temp = list()
-    temp2 = [[0] * N for _ in range(N)]
-    
-    answer = 0
+    board = []
+    result = 0
     
     for _ in range(N):
-        temp.append(list(map(int, input().split())))
+        board.append(list(map(int, input().split())))
         
-    for i in range(N):
-        for j in range(N):
-            temp2[i][j] = temp[j][i]
-    
-    for row in temp2:
-        temp.append(row)
-
-    count_list = list()
-        
-    for n in range(N*2):
+    for row in range(N):
         cnt = 0
         
-        for k in range(N):
-            if temp[n][k] == 1:
+        for col in range(N):
+            if board[row][col] == 1:
                 cnt += 1
             else:
                 if cnt == K:
-                    answer += 1
+                    result += 1
                 cnt = 0
         
-        count_list.append(cnt)
+        if cnt == K:
+            result += 1
+                
+    for col in range(N):
+        cnt = 0
+        
+        for row in range(N):
+            if board[row][col] == 1:
+                cnt += 1
+            else:
+                if cnt == K:
+                    result += 1
+                cnt = 0
         
         if cnt == K:
-            answer += 1
-    
-    print(f"#{test_case} {answer}")
+            result += 1
+        
+    print(f'#{test_case} {result}')
