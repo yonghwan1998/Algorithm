b95707d38ba2ae0cffb706a3358dfdd8816e7a15
yongss <129483177+yonghwan1998@users.noreply.github.com>
2026-09-01 13:57:08 +0900

    [D2] Title: 파리 퇴치, Time: 69 ms, Memory: 59,520 KB -BaekjoonHub

--- SWEA/D2/2001. 파리 퇴치/파리 퇴치.py
+++ SWEA/D2/2001. 파리 퇴치/파리 퇴치.py
@@ -1,17 +1,23 @@
 T = int(input())
 
-for test_case in range(1, 1 + T):
+for test_case in range(1, T + 1):
     N, M = map(int, input().split())
-    answer = 0
+    board = []
+    cnt_fly = []
     
-    board = [list(map(int, input().split())) for _ in range(N)]
-    
-    for i in range(N - M + 1):
-        for j in range(N - M + 1):
+    for i in range(N):
+        board.append(list(map(int, input().split())))
+        
+    # for문, N - M + 1만큼 반복
+    for row in range(N - M + 1):
+        for col in range(N - M + 1):
+            # board[row][col] 위치에서 이중 for문, M만큼 반복
             temp = 0
-            for k in range(M):
-                for l in range(M):
-                    temp += board[i + k][j + l]
-            answer = max(answer, temp)
+            for i in range(M):
+                for j in range(M):
+                    temp += board[row + i][col + j]
+            cnt_fly.append(temp)
     
-    print(f"#{test_case} {answer}")
+    result = max(cnt_fly)
+    
+    print(f'#{test_case} {result}')
