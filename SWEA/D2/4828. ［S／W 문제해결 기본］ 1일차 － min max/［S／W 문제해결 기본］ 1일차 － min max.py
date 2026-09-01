66b7cd8f1e530da29226ac1b192397eb38bab8af
yongss <129483177+yonghwan1998@users.noreply.github.com>
2026-09-01 11:45:10 +0900

    [D2] Title: [S/W 문제해결 기본] 1일차 - min max, Time: 71 ms, Memory: 52,224 KB -BaekjoonHub

--- SWEA/D2/4828. ［S／W 문제해결 기본］ 1일차 － min max/［S／W 문제해결 기본］ 1일차 － min max.py
+++ SWEA/D2/4828. ［S／W 문제해결 기본］ 1일차 － min max/［S／W 문제해결 기본］ 1일차 － min max.py
@@ -1,11 +1,21 @@
 T = int(input())
 
-for test_case in range(1, 1 + T):
-    N = int(input())
+for test_case in range(1, T + 1):
+    result = 0
     
+    N = int(input())
     list_a = list(map(int, input().split()))
     
-    max_a = max(list_a)
-    min_a = min(list_a)
+    max_num, min_num = list_a[0], list_a[0]
     
-    print(f"#{test_case} {max_a - min_a}")
+    # for문으로 가장 큰 수와 가장 작은 수를 각각 저장
+    for n in list_a:
+        if n > max_num:
+            max_num = n
+        if n < min_num:
+            min_num = n
+    
+    # result에 가장 큰 수 - 가장 작은 수 저장
+    result = max_num - min_num
+    
+    print(f'#{test_case} {result}')
